0867f60afc8fbf60066a5ddfac6d672d4829bb5f
LeoKang <xeungjin@nate.com>
2024-06-05 12:35:10 +0900

    Servlet

---
+++ MustHaveJSP/src/main/java/servlet/AnnoMapping.java
@@ -0,0 +1,26 @@
+package servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class AnnoMapping
+ */
+@WebServlet("/13Servlet/AnnoMapping.do")
+public class AnnoMapping extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setAttribute("message", "@WebServlet으로 매핑");
+		request.getRequestDispatcher("/13Servlet/AnnoMapping.jsp").forward(request, response);
+	}
+}
